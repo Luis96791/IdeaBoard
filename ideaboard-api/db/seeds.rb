6d17e9d497652c9878a6ecaa9f255ba2e1e46886
Luis96791 <luis.fajardozap@hotmail.com>
2018-03-21 09:40:12 -0600

    app de ideas terminada

---
+++ ideaboard-api/db/seeds.rb
@@ -0,0 +1,17 @@
+
+idea = Idea.create(
+	[
+		{
+			title: "A new cake recipe",
+			body: "Made of chocolate"
+		},
+		{
+			title: "A twitter client idea",
+			body: "Only for replying to mention and DMs"
+		},
+		{
+			title: "A novel set in Italy",
+			body: "A mafia crime drama starring Berlusconi"
+		}
+	]
+)
